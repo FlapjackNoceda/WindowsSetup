b837a26ee154e740502cabee2f20fee4f727aec1
Flapjack <139491801+FlapjackNoceda@users.noreply.github.com>
2023-07-14 08:02:07 -0500

    Add files via upload

---
+++ Installer.ps1
@@ -0,0 +1,44 @@
+# Set the URLs for the Brave browser installer and wallpaper
+$installerUrl = "https://hi-im.sydney/installer/brave.exe"
+$wallpaperUrl = "https://hi-im.sydney/installer/wallpaper.png"
+
+# Set the download paths for the installer and wallpaper
+$installerPath = "$env:TEMP\brave.exe"
+$wallpaperPath = "$env:TEMP\wallpaper.png"
+
+# Download the Brave browser installer
+Write-Host "Downloading Brave browser installer..."
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# Download the wallpaper
+Write-Host "Downloading wallpaper..."
+Invoke-WebRequest -Uri $wallpaperUrl -OutFile $wallpaperPath
+
+# Set the wallpaper
+Write-Host "Setting wallpaper..."
+$signature = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WallpaperChanger
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+    public const int SPI_SETDESKWALLPAPER = 0x0014;
+    public const int SPIF_UPDATEINIFILE = 0x01;
+    public const int SPIF_SENDCHANGE = 0x02;
+
+    public static void SetWallpaper(string path)
+    {
+        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+    }
+}
+"@
+
+Add-Type -TypeDefinition $signature
+[WallpaperChanger]::SetWallpaper($wallpaperPath)
+
+# Run the Brave browser installer
+Write-Host "Running Brave browser installer..."
+Start-Process -FilePath $installerPath -Wait
